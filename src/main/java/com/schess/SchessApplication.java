a3668d6764ce490e9b4f91fbb69b6f2b3acee76c
a18878099 <OSeSergienko@sberbank.ru>
2021-08-17 15:41:47 +0300

    first commit

---
+++ src/main/java/com/schess/SchessApplication.java
@@ -0,0 +1,13 @@
+package com.schess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/schess/SchessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
