a3accda10a4579226740bb20c39400e8905c59eb
a18878099 <OSeSergienko@sberbank.ru>
2021-09-06 18:03:50 +0300

    Все поменял

--- src/main/java/com/schess/models/AbstractEntity.java
+++ src/main/java/com/schess/models/AbstractEntity.java
@@ -2,18 +2,15 @@ package com.schess.models;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 
-@Entity
+@MappedSuperclass
 @Data
-public class Users {
+public abstract class AbstractEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
-
-    private String name;
-    private String email;
-}
+    private Integer id;
+ }
